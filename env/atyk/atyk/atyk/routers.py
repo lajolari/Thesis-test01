9de81f1b8fab14cbadf335ceff898297323087d6
Leonardo <leolama18@gmail.com>
2018-08-03 12:29:27 -0400

    progress (?)

--- env/atyk/atyk/atyk/routers.py
+++ env/atyk/atyk/atyk/routers.py
@@ -1,4 +1,5 @@
 from rest_framework import routers
-from search.viewsets import RecetaViewSet
+from search.viewsets import RecetaViewSet, IngredienteViewSet
 router = routers.DefaultRouter()
 router.register(r'addreceta', RecetaViewSet)
+router.register(r'addingrediente', IngredienteViewSet)
--- env/atyk/atyk/search/serializers.py
+++ env/atyk/atyk/search/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
-from .models import Receta
+from .models import Receta, Ingrediente
 class RecetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receta
+        fields = '__all__'
+
+class IngredienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingrediente
         fields = '__all__'
